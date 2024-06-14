0624411c90f2045fdb55112b4b43f005f4b84275
king-louis9 <kingsley6294@gmail.com>
2024-06-14 23:24:08 +0000

    Python last digit commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,9 +9,9 @@ else:
     las_dgt = number % 10
 
 if las_dgt > 5:
-    print(f"Last digit of {number} is {las_dgt} and is greater than 5")
+    print("Last digit of {number} is {las_dgt} and is greater than 5")
 elif las_dgt == 0:
     print(f"Last digit of {number} is {las_dgt} and is 0")
 else:
     print(f"Last digit of {number} is {las_dgt} and is less than 6 and not 0")
-    
+
